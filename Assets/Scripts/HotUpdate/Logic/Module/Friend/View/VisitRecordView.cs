e9a2f6a04d62e49835ed57255c09ed23ec211fcb
DESKTOP-KJKMO1K\kincaid <49565458@qq.com>
2025-12-11 12:11:04 +0800

    增加采集记录界面

--- Assets/Scripts/HotUpdate/Logic/Module/Friend/View/VisitRecordView.cs
+++ Assets/Scripts/HotUpdate/Logic/Module/Friend/View/VisitRecordView.cs
@@ -20,7 +20,7 @@ public class VisitRecordView : BaseWindow
     {
         base.OnInit();
         view = ui as fun_Friends.VisitRecordView;
-
+        SetBg(view.bg, "Friend/ELIDA_haoyou_haoyoubg.png");
         EventManager.Instance.AddEventListener(FriendEvent.FriendStealMesg, FriendStealCallBack);
         view.list.SetVirtual();
         view.list.itemRenderer = ItemRender;
@@ -33,11 +33,12 @@ public class VisitRecordView : BaseWindow
     }
     private void ItemRender(int index, GObject item) {
         fun_Friends.VisitRecordItem ui_ = item as fun_Friends.VisitRecordItem;
+        view.status.selectedIndex = stealMesData.Count > 0 ? 0 : 1;
         if (index >= 0 && index < stealMesData.Count)
         {
             var stealMesItem= stealMesData[index];
             uint userId = stealMesItem.targetUserId;
-            var friendData = FriendModel.Instance.GetFriendData(userId);
+            var friendData = stealMesItem.userInfo;
             if(friendData!=null)
             {
                 //StringUtil.SetBtnUrl(ui_.picFrame, "Avatar/ELIDA_common_touxiangdi01.png");
@@ -45,10 +46,16 @@ public class VisitRecordView : BaseWindow
                 ui_.txt_name.text = friendData.townName;
                 ui_.txt_daysVisit.text= stealMesItem.reqTime.ToString();
                 StringUtil.SetBtnUrl(ui_.btn_newApply, "»Ø·Ã");
+                ui_.btn_newApply.onClick.Add(OnVisitFriend);
+                ui_.n20.visible= friendData.canSteal;
             }
         }
     }
-
+    public void OnVisitFriend(EventContext context)
+    {
+        var friendData = (context.sender as GComponent).parent.data as I_FRIEND_PROFILE;
+        FriendController.Instance.ReqFriendVisit(friendData.userId);
+    }
     public override void OnShown()
     {
         base.OnShown();
